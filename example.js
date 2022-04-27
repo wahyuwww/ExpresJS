6d37925571b5f74bdb5449d950727cea390f38ec
wahyuwww <wahyu.aan2508@gmail.com>
2022-04-27 16:42:38 +0700

    update

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 const { genSalt } = require('bcrypt')
-const bcrypt = require('bcrypt')
+// const bcrypt = require('bcrypt')
 const coba = require('./helpers/coba.js')
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -31,15 +31,15 @@ app.listen(port, () => {
 //   res.send('Hello Dunia')
 // });
 
-const saltRounds = 10
-const myPlaintextPassword = 'admin'
+// const saltRounds = 10
+// const myPlaintextPassword = 'admin'
 
-bcrypt.genSalt(saltRounds, function (err, salt) {
-  bcrypt.hash(myPlaintextPassword, salt, function (err, hash) {
-    // Store hash in your password DB.
-
-  })
-})
+// bcrypt.genSalt(saltRounds, function (err, salt) {
+//   bcrypt.hash(myPlaintextPassword, salt, function (err, hash) {
+//     // Store hash in your password DB.
+//     console.log(hash)
+//   })
+// })
 
 genSalt()
   .then((result) => {
